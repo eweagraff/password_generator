57ed7f54f86cec8270a948f6974911040ea4c3e2
eweagraff <weagraff.emily@gmail.com>
2021-04-07 22:11:18 -0700

    added variables to JS file

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaeArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacterArray = ["!", "@",  "#", "$", "%", "^", "&", "*"];
+var userInput;
+var chooserUpperCase;
+var chooseLowerCase;
+var chooseNumbers;
+var chooseSpecialCharacter;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
